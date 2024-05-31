f8e60ba8e5411630d512d0dcd54bf60df4dd861c
cbillard <cbillard@com-company.fr>
2024-05-31 17:47:56 +0200

    implement complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
-    const loggedIn = { firstName: 'Cédric', lastName: 'Boulet'}
+    const loggedIn = { firstName: 'Cédric', lastName: 'BILLARD', email: 'cbillard@gmail.com'}
     return (
         <section className='home'>
             <div className='home-content'>
@@ -20,7 +21,13 @@ const Home = () => {
                         totalCurrentBalance={1250.35}
                     />
                 </header>
+                RECENT TRANSACTIONS
             </div>
+            <RightSidebar 
+                user={loggedIn}
+                transactions={[]}
+                banks={[{currentBalance: 123.50}, {currentBalance: 500.50}]}
+            />
         </section>
     )
 }
--- constants/index.ts
+++ constants/index.ts
@@ -2,22 +2,22 @@ export const sidebarLinks = [
   {
     imgURL: "/icons/home.svg",
     route: "/",
-    label: "Home",
+    label: "Accueil",
   },
   {
     imgURL: "/icons/dollar-circle.svg",
     route: "/my-banks",
-    label: "My Banks",
+    label: "Mes Banques",
   },
   {
     imgURL: "/icons/transaction.svg",
     route: "/transaction-history",
-    label: "Transaction History",
+    label: "Historique des Transactions",
   },
   {
     imgURL: "/icons/money-send.svg",
     route: "/payment-transfer",
-    label: "Transfer Funds",
+    label: "Transferer des Fonds",
   },
 ];
 
